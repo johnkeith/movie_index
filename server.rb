9332b38731d34e91135a1c73c821ce2359405c3c
John Keith <johnkeith@outlook.com>
2014-05-22 21:31:03 -0400

    init commit

--- server.rb
+++ server.rb
@@ -10,13 +10,20 @@ def read_movies_csv(csv)
   all_movies
 end
 
+all_movies = read_movies_csv('movies.csv').sort_by { |row| row[:title] }
 
 get '/' do
   "Hello Index"
 end
 
 get '/movies' do
-  @all_movies = read_movies_csv('movies.csv')
+  @all_movies = all_movies
+  erb :movies
+end
+
+get '/movies/:movie_id' do
+  @movie_id = params[:movie_id]
+  @movie = all_movies.select { |movie| movie[:id] == @movie_id }
   binding.pry
-  "Hello Movies"
+  erb :movie
 end
