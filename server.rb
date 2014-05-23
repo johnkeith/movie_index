cb1699981233f869483bd43dcce82e76506dd242
John Keith <johnkeith@outlook.com>
2014-05-23 10:08:14 -0400

    pages done

--- server.rb
+++ server.rb
@@ -10,6 +10,14 @@ def read_movies_csv(csv)
   all_movies
 end
 
+def pagination(model, page, offset)
+  if page == 1
+    model = model[offset..offset + 20]
+  else
+    model = model[offset + 1..offset + 20]
+  end
+end
+
 all_movies = read_movies_csv('movies.csv').sort_by { |row| row[:title] }
 
 get '/' do
@@ -17,13 +25,15 @@ get '/' do
 end
 
 get '/movies' do
-  @all_movies = all_movies
+  @page = params[:page] || 1
+  @page = @page.to_i
+  offset = ((@page - 1) * 20)
+  @all_movies = pagination(all_movies, @page, offset)
   erb :movies
 end
 
 get '/movies/:movie_id' do
   @movie_id = params[:movie_id]
   @movie = all_movies.select { |movie| movie[:id] == @movie_id }
-  binding.pry
   erb :movie
 end
